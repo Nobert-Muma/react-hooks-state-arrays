772b37f56fde84f04b420014e45c32b277ad2079
Nobert-Muma <mumanobert17@gmail.com>
2024-08-02 13:40:18 +0300

    Added, removed, updated elements in arrays in state and conditionally rendered elements from elements in state

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,20 +3,58 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
-
+  const [filterBy, setFilterBy]=useState("All");
+  console.log(filterBy);
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    setFoods([...foods, newFood]);
     console.log(newFood);
   }
+  function handleRemove(id){
+    const newFoodArray=foods.filter((food)=>food.id!==id);
+    setFoods(newFoodArray);
+  }
+  function handleUpdate(id){
+    const newFoodArray=foods.map((food)=>{
+      if(food.id===id){
+        return {
+          ...foods,
+          heatLevel:food.heatLevel+1,
+        };
+      } else {
+        return food;
+      }
+    })
+    setFoods(newFoodArray);
+  }
+  function handleFilterChange(event){
+    setFilterBy(event.target.value);
+  }
+  const foodsToDisplay=foods.filter((food)=>{
+    if(filterBy==="All"){
+      return true;
+    } else {
+      return food.cuisine===filterBy;
+    }
+  })
 
-  const foodList = foods.map((food) => (
+  const foodList = foodsToDisplay.map((food) => (
     <li key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      <button onClick={()=>handleUpdate(food.id)}>Update</button>
+      <button onClick={()=>handleRemove(food.id)}>Remove</button>
     </li>
   ));
 
   return (
     <div>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
